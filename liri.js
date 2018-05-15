7f43fb6d9cf4522b58a3d865879e74eea60f29ef
Keely Muench <kmuench13@gmail.com>
2018-05-14 22:52:14 -0500

    liri.js

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
 var keys = require("./keys.js")
 var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var request = require("request");
 require("dotenv").config();
 
 // var spotify = new Spotify(keys.spotify);
@@ -18,4 +20,19 @@ if (food[2] === "my-tweets") {
         }
       
       });
-}
+} else if (food[2] === "spotify-this-song") {
+
+ 
+var spotify = new Spotify({
+  id: "dbb7d0f3402d4a5da336696d00135680",
+  secret: "13228f41ce554efc807c8ae4d884865c"
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+};
